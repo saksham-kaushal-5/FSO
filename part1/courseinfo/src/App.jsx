f2ce9035584f7ec4f9a0ce92047e293c26f7572f
Saksham Kaushal <saksham.kaushal.official@gmail.com>
2024-12-23 11:49:33 +0530

    1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,96 +1,47 @@
 /* eslint-disable react/prop-types */
-import './App.css'
+import './App.css';
 
-const Header = ({course}) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  )
-}
+const Header = ({ course }) => (
+  <div>
+    <h1>{course}</h1>
+  </div>
+);
 
-// function Part({ name, exercise }) {
-//   return <p>{part} {exercise}</p>
-// }
+const Part = ({ part, exercise }) => (
+  <p>{part} {exercise}</p>
+);
 
-const Part = ({name, exercise}) => {
-  return <p>{name} {exercise}</p>
-}
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((ele, index) => (
+      <Part key={index} part={ele.part} exercise={ele.exercise} />
+    ))}
+  </div>
+);
 
-// function Content({ content }) {
-//   return (
-//     <div>
-//       {content.map((ele, index) => (
-//         <Part key={index} part={ele.part} exercise={ele.exercise} />
-//       ))}
-//     </div>
-//   )
-// }
-
-const Content = ({part1, part2, part3}) => {
-  return (
-    <>
-      <Part name={part1.name} exercise={part1.exercises}/>
-      <Part name={part2.name} exercise={part2.exercises}/>
-      <Part name={part3.name} exercise={part3.exercises}/>
-    </> 
-  )
-
-}
-
-// function Total({ exercises }) {
-//   const sum = exercises.reduce((total, ele) => total + ele, 0)
-//   return <p>Number of exercises {sum}</p>
-// }
-
-const Total = ({part1, part2, part3}) => {
-  return (
-    <p>No of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-  )
-}
-
-// function App() {
-//   const course = 'Half Stack application development'
-//   const content = [
-//     { part: 'Fundamentals of React', exercise: 10 },
-//     { part: 'Using props to pass data', exercise: 7 },
-//     { part: 'State of a component', exercise: 14 }
-//   ]
-
-//   // Directly pass the content's exercise values to Total
-//   const exercises = content.map(item => item.exercise)
-
-//   return (
-//     <div>
-//       <Header course={course} />
-//       <Content content={content} />
-//       <Total exercises={exercises} />
-//     </div>
-//   )
-// }
+const Total = ({ exercises }) => {
+  const sum = exercises.reduce((total, ele) => total + ele, 0);
+  return <p>Number of exercises {sum}</p>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = 'Half Stack application development';
+  const parts = [
+    { part: 'Fundamentals of React', exercise: 10 },
+    { part: 'Using props to pass data', exercise: 7 },
+    { part: 'State of a component', exercise: 14 },
+  ];
+
+  // Directly pass the content's exercise values to Total
+  const exercises = parts.map(item => item.exercise);
 
   return (
-  <div>
-    <Header course={course} />
-    <Content part1 = {part1} part2 = {part2} part3 = {part3}/>
-    <Total part1 = {part1} part2 = {part2} part3 = {part3}/>
-  </div>
-  )
-}
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises={exercises} />
+    </div>
+  );
+};
 
-export default App
+export default App;
